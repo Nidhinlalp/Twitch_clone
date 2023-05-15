664c7bf0084d63e67de6661258a38bdfc919c0e8
Nidhin <lalnidhinp02@gmail.com>
2023-05-15 13:38:58 +0530

    logi screen is creanted

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:twithc_clone/utils/size.dart';
+import 'package:twithc_clone/widgets/custom_button.dart';
+import 'package:twithc_clone/widgets/custom_textfield.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String routeName = '/login';
@@ -9,8 +12,60 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _userNameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: size.height * 0.1),
+              const Text(
+                'Email',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: CustomTextField(
+                  controller: _emailController,
+                ),
+              ),
+              kHight20,
+              const Text(
+                'Password',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 8.0,
+                ),
+                child: CustomTextField(
+                  controller: _passwordController,
+                ),
+              ),
+              kHight20,
+              CustomButton(
+                text: 'Login',
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
