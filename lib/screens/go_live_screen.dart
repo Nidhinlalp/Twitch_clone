ed52578fb6383f9827528998a7fdd2fcf53dc775
Nidhin <lalnidhinp02@gmail.com>
2023-05-15 17:41:05 +0530

    go live scfeen ui completed

--- lib/screens/go_live_screen.dart
+++ lib/screens/go_live_screen.dart
@@ -2,6 +2,8 @@ import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:twithc_clone/utils/colors.dart';
 import 'package:twithc_clone/utils/size.dart';
+import 'package:twithc_clone/widgets/custom_button.dart';
+import 'package:twithc_clone/widgets/custom_textfield.dart';
 
 class GoLiveScreen extends StatefulWidget {
   const GoLiveScreen({super.key});
@@ -11,50 +13,85 @@ class GoLiveScreen extends StatefulWidget {
 }
 
 class _GoLiveScreenState extends State<GoLiveScreen> {
+  final TextEditingController _titelController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titelController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 22.0, vertical: 10),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 22.0),
-              child: DottedBorder(
-                borderType: BorderType.RRect,
-                radius: const Radius.circular(10),
-                dashPattern: const [10, 4],
-                strokeCap: StrokeCap.round,
-                color: buttonColor,
-                child: Container(
-                  width: double.infinity,
-                  height: 150,
-                  decoration: BoxDecoration(
-                    color: buttonColor.withOpacity(.05),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Icon(
-                        Icons.folder_open_rounded,
-                        color: buttonColor,
-                        size: 40,
+              child: Column(
+                children: [
+                  DottedBorder(
+                    borderType: BorderType.RRect,
+                    radius: const Radius.circular(10),
+                    dashPattern: const [10, 4],
+                    strokeCap: StrokeCap.round,
+                    color: buttonColor,
+                    child: Container(
+                      width: double.infinity,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: buttonColor.withOpacity(.05),
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                      kHight10,
-                      Text(
-                        'Select your Thumbnail',
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.folder_open_rounded,
+                            color: buttonColor,
+                            size: 40,
+                          ),
+                          kHight10,
+                          Text(
+                            'Select your Thumbnail',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.grey.shade400,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  kHight20,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Title',
                         style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.grey.shade400,
                           fontWeight: FontWeight.bold,
                         ),
-                      )
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: CustomTextField(
+                          controller: _titelController,
+                        ),
+                      ),
                     ],
                   ),
-                ),
+                ],
               ),
-            )
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 0),
+              child: CustomButton(text: 'Go Live', onTap: () {}),
+            ),
           ],
         ),
       ),
