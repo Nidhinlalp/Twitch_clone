e33b3eb15316b665daa5d77984fbe73ff9d14fc9
Nidhin <lalnidhinp02@gmail.com>
2023-05-15 13:12:50 +0530

    onbord screen ui completed

--- lib/screens/onbording_screen.dart
+++ lib/screens/onbording_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:twithc_clone/utils/size.dart';
+import 'package:twithc_clone/widgets/custom_button.dart';
 
 class OnboardingScreen extends StatelessWidget {
   static const routeName = '/onboarding';
@@ -8,26 +9,38 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          Text(
-            'Welcome to',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 40,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Welcome to',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+              ),
             ),
-          ),
-          Text(
-            'Twitch',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 40,
+            const Text(
+              'Twitch',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+              ),
             ),
-          ),
-          kHight20,
-        ],
+            kHight20,
+            CustomButton(
+              text: 'Log in',
+              onTap: () {},
+            ),
+            kHight10,
+            CustomButton(
+              text: 'Sign Up',
+              onTap: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
