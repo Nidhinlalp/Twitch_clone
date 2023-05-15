a88b23f858a0ef83a6d03b364a36ebd4b6b27cb9
Nidhin <lalnidhinp02@gmail.com>
2023-05-15 17:13:06 +0530

    bottom navigation bar ready

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:twithc_clone/screens/feed_screen.dart';
+import 'package:twithc_clone/screens/go_live_screen.dart';
+import 'package:twithc_clone/utils/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   static String routeName = '/home';
@@ -9,8 +12,53 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _page = 0;
+  List<Widget> pages = [
+    const FeedScreen(),
+    const GoLiveScreen(),
+    const Center(
+      child: Text('Browser'),
+    )
+  ];
+
+  onPageChange(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: buttonColor,
+        unselectedItemColor: primaryColor,
+        backgroundColor: backgroundColor,
+        unselectedFontSize: 12,
+        onTap: onPageChange,
+        currentIndex: _page,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite,
+            ),
+            label: 'Flollowing',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add_rounded,
+            ),
+            label: 'Go Live',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.copy,
+            ),
+            label: 'Browsing',
+          ),
+        ],
+      ),
+      body: pages[_page],
+    );
   }
 }
