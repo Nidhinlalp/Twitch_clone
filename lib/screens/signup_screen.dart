0afe80cf6c5a7287cb369df833810d041a57c1ca
Nidhin <lalnidhinp02@gmail.com>
2023-05-15 13:36:22 +0530

    signup screen is completed,and create the custom textfield widget

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:twithc_clone/utils/size.dart';
+import 'package:twithc_clone/widgets/custom_button.dart';
+import 'package:twithc_clone/widgets/custom_textfield.dart';
 
 class SignUpScreen extends StatefulWidget {
   static const String routeName = '/signup';
@@ -9,8 +12,75 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _userNameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: size.height * 0.1),
+              const Text(
+                'Email',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: CustomTextField(
+                  controller: _emailController,
+                ),
+              ),
+              kHight20,
+              const Text(
+                'Username',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 8.0,
+                ),
+                child: CustomTextField(
+                  controller: _userNameController,
+                ),
+              ),
+              kHight20,
+              const Text(
+                'Password',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 8.0,
+                ),
+                child: CustomTextField(
+                  controller: _passwordController,
+                ),
+              ),
+              kHight20,
+              CustomButton(
+                text: 'Sign Up',
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
